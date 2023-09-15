a8750d7c2f360b5bbd5d2452444a2a8549165bae
Øystein Tveito <oystein.tveito@uit.no>
2023-09-15 15:15:19 +0200

    created popup

---
+++ components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+const SplashScreen = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const shouldShowPopup = localStorage.getItem('shouldShowPopup');
+    if (shouldShowPopup === null || shouldShowPopup === 'true') {
+      setShowPopup(true);
+    }
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleCheckboxChange = (e) => {
+    localStorage.setItem('shouldShowPopup', String(!e.target.checked));
+  };
+
+  if (!showPopup) {
+    return null;
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-white rounded-lg p-4 max-w-lg w-full mx-auto dark:bg-[#202123]">
+        <div className="text-lg pb-4 font-bold text-black dark:text-neutral-200">
+          Welcome to My App
+        </div>
+        <div className="text-sm font-bold mb-2 text-black dark:text-neutral-200">
+          Show this next time?
+        </div>
+        <label className="w-full cursor-pointer bg-transparent p-2 text-neutral-700 dark:text-neutral-200">
+          <input type="checkbox" onChange={handleCheckboxChange} />
+          Don't show this again
+        </label>
+        <button
+          type="button"
+          className="w-full px-4 py-2 mt-6 border rounded-lg shadow border-neutral-500 text-neutral-900 hover:bg-neutral-100 focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-white dark:text-black dark:hover:bg-neutral-300"
+          onClick={handleClosePopup}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SplashScreen;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,19 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import SplashScreen from '@/components/SplashScreen/SplashScreen';
 
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
   return (
     <div className={inter.className}>
+      <SplashScreen />
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
