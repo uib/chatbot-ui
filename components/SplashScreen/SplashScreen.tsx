6ec2b3a17326b36c24c30efce0a6518079e678a7
Øystein Tveito <oystein.tveito@uit.no>
2023-09-18 10:01:01 +0200

    added popup

--- components/SplashScreen/SplashScreen.tsx
+++ components/SplashScreen/SplashScreen.tsx
@@ -26,14 +26,15 @@ const SplashScreen = () => {
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-4 max-w-lg w-full mx-auto dark:bg-[#202123]">
         <div className="text-lg pb-4 font-bold text-black dark:text-neutral-200">
-          Welcome to My App
+          ChatUiT
         </div>
-        <div className="text-sm font-bold mb-2 text-black dark:text-neutral-200">
-          Show this next time?
+        <div className="text-sm mb-2 text-black dark:text-neutral-200">
+          Her er informasjon om ChatUiT...
         </div>
+        <br></br>
         <label className="w-full cursor-pointer bg-transparent p-2 text-neutral-700 dark:text-neutral-200">
           <input type="checkbox" onChange={handleCheckboxChange} />
-          Don't show this again
+          <a> </a>Don't show this again
         </label>
         <button
           type="button"
